0d5d84e3d3358db398c9760c98a2bbcf39d040b1
Darrell Pawson <dpawson905@gmail.com>
2018-11-07 18:19:45 -0500

    User Auth - Test postRegister Method

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,19 @@
+const debug = require('debug')('code-w-node:register');
 const User = require('../models/user');
 
 module.exports = {
   // POST register
   postRegister(req, res, next) {
-    res.send('POST REGISTER')
+    debug('Registering User');
+    User.register(new User({
+      username: req.body.username
+    }), req.body.password, (err) => {
+      if(err) {
+        debug('ERROR WHILE REGISTERING USER', err)
+        return next(err);
+      }
+      debug('User registered!')
+      res.redirect('/')
+    })
   }
 }
