694b670ee5291031a1d0a8873e429121c3d53cd7
Darrell Pawson <dpawson905@gmail.com>
2018-11-07 20:48:03 -0500

    User Auth - Add User Email & Profile Image

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,12 @@ module.exports = {
   // POST register
   postRegister(req, res, next) {
     debug('Registering User');
-    User.register(new User({
-      username: req.body.username
-    }), req.body.password, (err) => {
+    const newUser = new User({
+      email: req.body.email,
+      username: req.body.username,
+      image: req.body.image
+    });
+    User.register(newUser, req.body.password, (err) => {
       if(err) {
         debug('ERROR WHILE REGISTERING USER', err)
         return next(err);
