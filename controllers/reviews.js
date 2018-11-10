9f7b8b0bb92390ec173c74ee75c2e82839634552
Darrell Pawson <dpawson905@gmail.com>
2018-11-10 10:51:33 -0500

    Reviews - Initial Setup

---
+++ controllers/reviews.js
@@ -0,0 +1,19 @@
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+module.exports = {
+  // Review Create
+  async reviewCreate(req, res, next) {
+    
+  },
+
+  // Review Update
+  async reviewUpdate(req, res, next) {
+    
+  },
+
+  // Review DESTROY
+  async reviewDestroy(req, res, next) {
+    
+  }
+}
