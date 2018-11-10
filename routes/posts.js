115eba6de93d4dabf9a3442a6b81618d6e72b332
Darrell Pawson <dpawson905@gmail.com>
2018-11-09 19:01:43 -0500

    Posts - Images Upload - Edit/Update

--- routes/posts.js
+++ routes/posts.js
@@ -37,7 +37,7 @@ router.get('/:id', asyncErrorHandler(postShow));
 router.get('/:id/edit', asyncErrorHandler(postEdit));
 
 /* PUT posts edit /posts/:id */
-router.put('/:id', asyncErrorHandler(postUpdate));
+router.put('/:id', upload.array('images', 4), asyncErrorHandler(postUpdate));
 
 /* DELETE posts edit /posts/:id */
 router.delete('/:id', asyncErrorHandler(postDestroy));
