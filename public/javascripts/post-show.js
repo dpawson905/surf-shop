65b8d159dc1598c6d341551299a08759db59d393
Darrell Pawson <dpawson905@gmail.com>
2018-11-10 12:19:58 -0500

    Reviews - Cleanup of Client JS

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -19,3 +19,11 @@ new mapboxgl.Marker(el)
     }) // add popups
     .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
   .addTo(map);
+
+// Toggle edit review form
+$('.toggle-edit-form').on('click', function () {
+  // toggle the edit button text on click
+  $(this).text() === 'Edit' ? $(this).text('Cancel') : $(this).text('Edit');
+  // toggle visibility of the edit review form
+  $(this).siblings('.edit-review-form').toggle();
+});
