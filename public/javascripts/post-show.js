6162d12ab94aa2af44957b089ae97f23300bbb67
Darrell Pawson <dpawson905@gmail.com>
2018-11-10 08:58:46 -0500

    Layouts - Show

---
+++ public/javascripts/post-show.js
@@ -0,0 +1,21 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHBhd3NvbjkwNSIsImEiOiJjam1ucXMzdnYwZmFkM3BsdWh3cTducmFnIn0.EOJQd5gfP0xCZOaAD41big';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: post.coordinates,
+  zoom: 14
+});
+
+// create a HTML element for the post location marker
+var el = document.createElement('div');
+el.className = 'marker';
+
+// make a marker for the location and add to the map
+new mapboxgl.Marker(el)
+  .setLngLat(post.coordinates)
+  .setPopup(new mapboxgl.Popup({
+      offset: 25
+    }) // add popups
+    .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
+  .addTo(map);
