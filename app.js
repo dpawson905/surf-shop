be0b66da5be8b25b9e3ad2ed2ff2016d4e11f290
Darrell Pawson <dpawson905@gmail.com>
2018-11-10 10:30:09 -0500

    Layouts - Flash Messages - Part 1

--- app.js
+++ app.js
@@ -58,9 +58,17 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Set title middleware
+// Set local variables  middleware
 app.use(function (req, res, next) {
-  res.locals.title = 'Surf Shop'
+  // set default page title
+  res.locals.title = 'Surf Shop';
+  // set success flash message
+  res.locals.success = req.session.success || '';
+  delete req.session.success;
+  // set error flash message
+  res.locals.error = req.session.error || '';
+  delete req.session.error;
+  // continue on to the next function in the middleware chain
   next()
 })
 
@@ -75,13 +83,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  debug(err)
+  req.session.error = err.message;
+  res.redirect('back');
+  // // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
 });
 
 module.exports = app;
