257ee8fcc04699bd774e28bd3ec7d0cc58565ec7
Darrell Pawson <dpawson905@gmail.com>
2019-03-04 15:37:32 -0500

    client side image upload

--- models/user.js
+++ models/user.js
@@ -8,7 +8,13 @@ const UserSchema = new Schema({
     unique: true,
     lowercase: true
   },
-  image: String
+  image: {
+    secure_url:{
+      type: String,
+      default: '/images/default-profile.jpg'
+    } ,
+    public_id: String
+  }
 });
 
 UserSchema.plugin(passportLocalMongoose);
