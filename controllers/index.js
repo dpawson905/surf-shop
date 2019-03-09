185361460ef896997858d33cbe9381fe5aebfccd
Darrell Pawson <dpawson905@gmail.com>
2019-03-09 17:45:19 -0500

    password reset and update

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
+const mapBoxToken = process.env.MAPBOX_API;
+
 const debug = require('debug')('code-w-node:register');
 const User = require('../models/user');
 const Post = require('../models/post');
-const passport = require('passport');
 const util = require('util');
 const { cloudinary } = require('../cloudinary');
-const { deleteProfileImage} = require('../middleware');
-
-const mapBoxToken = process.env.MAPBOX_API;
+const { deleteProfileImage } = require('../middleware');
+const crypto = require('crypto');
+const apiKey = process.env.MAILGUN_API_KEY;
+const domain = 'sandbox6d4eb8802e9b440cb1b20acd2ebeec27.mailgun.org';
+const mailgun = require('mailgun-js')({ apiKey, domain });
 
 module.exports = {
   // GET /
@@ -31,12 +34,12 @@ module.exports = {
   // POST /register
   async postRegister(req, res, next) {
     try {
-      if(req.file) {
-        const { secure_url, public_id} = req.file;
+      if (req.file) {
+        const { secure_url, public_id } = req.file;
         req.body.image = {
           secure_url,
           public_id
-        }
+        };
       }
       const user = await User.register(new User(req.body), req.body.password);
       req.login(user, function(err) {
@@ -103,25 +106,112 @@ module.exports = {
   },
 
   async updateProfile(req, res, next) {
-    const {
-      username,
-      email
-    } = req.body;
+    const { username, email } = req.body;
     const { user } = res.locals;
     if (username) user.username = username;
     if (email) user.email = email;
     if (req.file) {
-      if (user.image.public_id) await cloudinary.v2.uploader.destroy(user.image.public_id);
+      if (user.image.public_id)
+        await cloudinary.v2.uploader.destroy(user.image.public_id);
       const { secure_url, public_id } = req.file;
       user.image = {
         secure_url,
         public_id
-      }
+      };
     }
     await user.save();
     const login = util.promisify(req.login.bind(req));
     await login(user);
     req.session.success = 'Profile successfully updated!';
     res.redirect('/profile');
+  },
+  getForgotPw(req, res, next) {
+    res.render('users/forgot');
+  },
+  async putForgotPw(req, res, next) {
+    const token = await crypto.randomBytes(20).toString('hex');
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      req.session.error = 'No account with that email address exists.';
+      return res.redirect('/forgot-password');
+    }
+
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+    await user.save();
+
+    const mailData = {
+      from: 'Surf Shop Admin <dpawson905@gmail.com>',
+      to: user.email,
+      subject: 'Surf Shop - Forgot Password / Reset',
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.
+        Please click on the following link, or copy and paste it into your browser to complete the process:
+        http://${req.headers.host}/reset/${token}
+        If you did not request this, please ignore this email and your password will remain unchanged.`.replace(
+        /				/g,
+        ''
+      )
+    };
+
+    await mailgun.messages().send(mailData);
+    req.session.success = `An e-mail has been sent to ${
+      user.email
+    } with further instructions.`;
+    res.redirect('/forgot-password');
+  },
+  async getReset(req, res, next) {
+    const { token } = req.params;
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+    if (!user) {
+      req.session.error = 'Password reset token is invalid or has expired.';
+      return res.redirect('/forgot-password');
+    }
+    res.render('users/reset', { token });
+  },
+  async putReset(req, res, next) {
+    const { token } = req.params;
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) {
+      req.session.error = 'Password reset token is invalid or has expired.';
+      return res.redirect(`/reset/${token}`);
+    }
+
+    if (req.body.password === req.body.confirm) {
+      await user.setPassword(req.body.password);
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpires = undefined;
+      await user.save();
+      const login = util.promisify(req.login.bind(req));
+      await login(user);
+    } else {
+      req.session.error = 'Passwords do not match.';
+      return res.redirect(`/reset/${token}`);
+    }
+
+    const mailData = {
+      from: 'Surf Shop Admin <dpawson905@gmail.com>',
+      to: user.email,
+      subject: 'Surf Shop - Password Changed',
+      text: `Hello,
+        This email is to confirm that the password for your account has just been changed.
+        If you did not make this change, please hit reply and notify us at once.`.replace(
+        /		  	/g,
+        ''
+      )
+    };
+
+    await mailgun.messages().send(mailData);
+
+    req.session.success = 'Password successfully updated!';
+    res.redirect('/');
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 // Import middleware
 const {
@@ -18,7 +21,11 @@ const {
   postLogin,
   getLogout,
   getProfile,
-  updateProfile
+  updateProfile,
+  getForgotPw,
+  putForgotPw,
+  getReset,
+  putReset
 } = require('../controllers');
 
 /* GET home page. */
@@ -28,7 +35,7 @@ router.get('/', asyncErrorHandler(landingPage));
 router.get('/register', getRegister);
 
 /* POST /register */
-router.post('/register', asyncErrorHandler(postRegister));
+router.post('/register', upload.single('image'), asyncErrorHandler(postRegister));
 
 /* GET /login */
 router.get('/login', getLogin);
@@ -44,30 +51,23 @@ router.get('/profile', isLoggedIn, asyncErrorHandler(getProfile));
 
 /* PUT /profile */
 router.put('/profile', 
-  isLoggedIn, 
+  isLoggedIn,
+  upload.single('image'),
   asyncErrorHandler(isValidPassword),
   asyncErrorHandler(changePassword),
   asyncErrorHandler(updateProfile)
 );
 
 /* GET /forgot-password */
-router.get('/forgot-pw', (req, res, next) => {
-  res.render('password');
-});
+router.get('/forgot-password', getForgotPw);
 
 /* PUT /forgot-password */
-router.put('/forgot-pw', (req, res, next) => {
-  res.send('GET /forgot-password')
-});
+router.put('/forgot-password', asyncErrorHandler(putForgotPw));
 
 /* GET /reset-password */
-router.get('/reset-pw/:token', (req, res, next) => {
-  res.send('GET /reset-password/:token')
-});
+router.get('/reset/:token', asyncErrorHandler(getReset));
 
 /* PUT /reset-password */
-router.put('/reset-pw/:token', (req, res, next) => {
-  res.send('PUT /reset-password/:token')
-});
+router.put('/reset/:token', asyncErrorHandler(putReset));
 
 module.exports = router;
